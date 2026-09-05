426963189f48ceacbb2f8e2ac75edc7f5eaaa2e2
smtdfc <admin.smtdfc@gmail.com>
2026-09-05 15:19:34 +0700

    feat: enhance shell setup with Oh My Zsh and additional plugins

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,3 +4,17 @@ sudo pacman -Sy --needed base-devel squashfs-tools dosfstools mtools arch-instal
 chmod +x $CURRENT_DIR/scripts/*
 mkdir -p $CURRENT_DIR/build/{pacman/cache,work,out}
 mkdir -p $CURRENT_DIR/testing/{pacman/repo,flatpak,hooks}
+mkdir -p $CURRENT_DIR/airootfs/etc/skel/{Documents,Downloads,Music,Pictures,Videos}
+
+
+if [ ! -d "$CURRENT_DIR/airootfs/etc/skel/.oh-my-zsh" ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh.git --depth 1 "$CURRENT_DIR/airootfs/etc/skel/.oh-my-zsh"
+fi
+
+if [ ! -d "$CURRENT_DIR/airootfs/etc/skel/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git --depth 1 "$CURRENT_DIR/airootfs/etc/skel/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "$CURRENT_DIR/airootfs/etc/skel/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git --depth 1 "$CURRENT_DIR/airootfs/etc/skel/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+fi
